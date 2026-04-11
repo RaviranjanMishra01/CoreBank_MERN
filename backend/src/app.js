690874bc36e0508b9e4bcd8e7042dcdf3ec4284a
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-04-11 23:32:17 +0530

    appjs files

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const app = express();
+
+
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+/*
+    - Routes for both authRouter and accountRouter  
+*/
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+
+
+app.use("/api/auth",authRouter)
+app.use("/api/accounts",accountRouter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
